583d780bdbb329be38b7b664e93c44abdb7cc030
Eduardo <edureisfaria@gmail.com>
2024-09-22 23:03:44 +0100

    Register Page

---
+++ lib/pages/regiter_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+import '../components/buttons/button.dart';
+import '../components/inputs/text_field.dart';
+
+class RegisterPage extends StatefulWidget {
+  final void Function() ontap;
+
+  const RegisterPage({super.key, required this.ontap});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController pwController = TextEditingController();
+  final TextEditingController pwConfirmController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              //Center Column
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //"Space"
+                const SizedBox(
+                  height: 50,
+                ),
+
+                //Logo
+                Icon(
+                  Icons.lock_open_rounded,
+                  size: 86,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+
+                //"Space"
+                const SizedBox(
+                  height: 50,
+                ),
+
+                //Welcome back
+                Text(
+                  "Bora lá criar uma continha!",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                //Email input
+                MyTextField(
+                  controller: nameController,
+                  hintText: "Enter Name",
+                  hide: false,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                //Email input
+                MyTextField(
+                  controller: emailController,
+                  hintText: "Enter Email",
+                  hide: false,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+
+                //Password input
+                MyTextField(
+                  controller: pwController,
+                  hintText: "Enter Password",
+                  hide: true,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+
+                //Password input
+                MyTextField(
+                  controller: pwConfirmController,
+                  hintText: "Confirm Password",
+                  hide: true,
+                ),
+
+                const SizedBox(
+                  height: 25,
+                ),
+                //Sign in Button
+                MyButton(
+                  text: "Registar",
+                  onTap: () {
+                    print("Estou a tentar registar...");
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                //Already a member Login Now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Já és um membro?",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    GestureDetector(
+                      onTap: widget.ontap,
+                      child: Text(
+                        "Entra por aqui!",
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
